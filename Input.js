eb5d7932264f10be4cbf70f418d3fb8fa785e9d8
Philip Viningston <41139390+ws7one@users.noreply.github.com>
2022-05-18 18:05:51 +0400

    Create Input.js

---
+++ Input.js
@@ -0,0 +1,238 @@
+[
+  {
+    "id": 1,
+    "level": 0,
+    "path": "/",
+    "display": "root"
+  },
+  {
+    "id": 2,
+    "level": 1,
+    "path": "/root",
+    "display": "folder1"
+  },
+  {
+    "id": 5,
+    "level": 2,
+    "path": "/root/folder1",
+    "display": "folder11"
+  },
+  {
+    "id": 11,
+    "level": 3,
+    "path": "/root/folder1/folder11",
+    "display": "folder1111"
+  },
+  {
+    "id": 23,
+    "level": 4,
+    "path": "/root/folder1/folder11/folder1111",
+    "display": "file1",
+    "type": "file"
+  },
+  {
+    "id": 12,
+    "level": 3,
+    "path": "/root/folder1/folder11",
+    "display": "folder1112"
+  },
+  {
+    "id": 24,
+    "level": 4,
+    "path": "/root/folder1/folder11/folder1112",
+    "display": "file2",
+    "type": "file"
+  },
+  {
+    "id": 6,
+    "level": 2,
+    "path": "/root/folder1",
+    "display": "folder12"
+  },
+  {
+    "id": 13,
+    "level": 3,
+    "path": "/root/folder1/folder12",
+    "display": "folder1121"
+  },
+  {
+    "id": 25,
+    "level": 4,
+    "path": "/root/folder1/folder12/folder1121",
+    "display": "file3",
+    "type": "file"
+  },
+  {
+    "id": 14,
+    "level": 3,
+    "path": "/root/folder1/folder12",
+    "display": "folder1122"
+  },
+  {
+    "id": 26,
+    "level": 4,
+    "path": "/root/folder1/folder12/folder1122",
+    "display": "file4",
+    "type": "file"
+  },
+  {
+    "id": 3,
+    "level": 1,
+    "path": "/root",
+    "display": "folder2"
+  },
+  {
+    "id": 7,
+    "level": 2,
+    "path": "/root/folder2",
+    "display": "folder21"
+  },
+  {
+    "id": 15,
+    "level": 3,
+    "path": "/root/folder2/folder21",
+    "display": "folder1211"
+  },
+  {
+    "id": 27,
+    "level": 4,
+    "path": "/root/folder2/folder21/folder1211",
+    "display": "file5",
+    "type": "file"
+  },
+  {
+    "id": 16,
+    "level": 3,
+    "path": "/root/folder2/folder21",
+    "display": "folder1212"
+  },
+  {
+    "id": 28,
+    "level": 4,
+    "path": "/root/folder2/folder21/folder1212",
+    "display": "file6",
+    "type": "file"
+  },
+  {
+    "id": 8,
+    "level": 2,
+    "path": "/root/folder2",
+    "display": "folder22"
+  },
+  {
+    "id": 17,
+    "level": 3,
+    "path": "/root/folder2/folder22",
+    "display": "folder1221"
+  },
+  {
+    "id": 29,
+    "level": 4,
+    "path": "/root/folder2/folder22/folder1221",
+    "display": "file7",
+    "type": "file"
+  },
+  {
+    "id": 18,
+    "level": 3,
+    "path": "/root/folder2/folder22",
+    "display": "folder1222"
+  },
+  {
+    "id": 30,
+    "level": 4,
+    "path": "/root/folder2/folder22/folder1222",
+    "display": "file8",
+    "type": "file"
+  },
+  {
+    "id": 4,
+    "level": 1,
+    "path": "/root",
+    "display": "folder3"
+  },
+  {
+    "id": 9,
+    "level": 2,
+    "path": "/root/folder3",
+    "display": "folder31"
+  },
+  {
+    "id": 19,
+    "level": 3,
+    "path": "/root/folder3/folder31",
+    "display": "folder1311"
+  },
+  {
+    "id": 31,
+    "level": 4,
+    "path": "/root/folder3/folder31/folder1311",
+    "display": "file9",
+    "type": "file"
+  },
+  {
+    "id": 20,
+    "level": 3,
+    "path": "/root/folder3/folder31",
+    "display": "folder1312"
+  },
+  {
+    "id": 32,
+    "level": 4,
+    "path": "/root/folder3/folder31/folder1312",
+    "display": "file10",
+    "type": "file"
+  },
+  {
+    "id": 10,
+    "level": 2,
+    "path": "/root/folder3",
+    "display": "folder32"
+  },
+  {
+    "id": 21,
+    "level": 3,
+    "path": "/root/folder3/folder32",
+    "display": "folder1321"
+  },
+  {
+    "id": 33,
+    "level": 4,
+    "path": "/root/folder3/folder32/folder1321",
+    "display": "file11",
+    "type": "file"
+  },
+  {
+    "id": 22,
+    "level": 3,
+    "path": "/root/folder3/folder32",
+    "display": "folder1322"
+  },
+  {
+    "id": 34,
+    "level": 4,
+    "path": "/root/folder3/folder32/folder1322",
+    "display": "file12",
+    "type": "file"
+  },
+  {
+    "id": 35,
+    "level": 3,
+    "path": "/root/folder1/folder11",
+    "display": "file100",
+    "type": "file"
+  },
+  {
+    "id": 36,
+    "level": 2,
+    "path": "/root/folder2",
+    "display": "file0123",
+    "type": "file"
+  },
+  {
+    "id": 37,
+    "level": 4,
+    "path": "/root/folder3/folder31/folder1311",
+    "display": "folder13111"
+  }
+]
